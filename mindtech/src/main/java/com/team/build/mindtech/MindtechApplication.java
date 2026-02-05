f7945c8bf07061a70cf537b5cfa1be017266b630
Lucas Barros Jorge <lucas.jorge@sptech.school>
2026-02-04 22:22:40 -0300

    feat: Projeto Base

---
+++ mindtech/src/main/java/com/team/build/mindtech/MindtechApplication.java
@@ -0,0 +1,13 @@
+package com.team.build.mindtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindtechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindtechApplication.class, args);
+	}
+
+}
